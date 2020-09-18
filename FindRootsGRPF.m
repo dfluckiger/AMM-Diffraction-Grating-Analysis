2bce415d724f119ad5d99624563d01cbd0ad7e81
dfluckiger <67163489+dfluckiger@users.noreply.github.com>
2020-09-18 11:33:46 -0600

    Add files via upload

--- FindRootsGRPF.m
+++ FindRootsGRPF.m
@@ -1,4 +1,4 @@
-function [Roots, fnVal, itts, RootIsMP] = FindRootsGRPF(N,Gparams,pltIt)
+function [Roots, fnVal, itts, RootIsMP,rts] = FindRootsGRPF(N,Gparams,pltIt)
 %       AMM_mainLambdaSweep
 %       Analytic Modal Method for TE and TM diffraction grating
 %       efficiency calculation of lamellar gratings.
@@ -45,7 +45,10 @@ if length(unique(Gparams.ei)) == 1    % have a uniform layer, roots are determin
          hold on;
          plot(1i*real(Roots)+imag(Roots),'+k');
          hold off;
-     end          
+	end
+	fnVal = zeros(N,1);
+	itts = zeros(N,1);
+	RootIsMP = zeros(N,1);
      return;
 end
 
@@ -53,9 +56,9 @@ end
 ni = sqrt(Gparams.ei);
 lambda = 2*pi/Gparams.k0;
 nrts = 0;
-r = lambda/3.75;					%  r      : initial mesh step
+r = lambda/5;					%  r      : initial mesh step
 r1 =max(real(ni))*1.5;	
-i2 = max(imag(ni))+N*lambda/9.5;
+i2 = max(imag(ni))+N*lambda/2;
 xb = -0.537;				   %  xb     : real part range begin 
 xe = 1.5*r1+3*r;			%  xe     : real part range end 
 yb = -0.2;				  %  yb     : imag part range begin 
@@ -63,10 +66,10 @@ ye = i2+3*r;				  %  ye     : imag part range end
 bc = [xb,xe,yb,ye,r];
 
 NewNodesCoord = rectdom(xb,xe,yb,ye,r);
-Tol = 1e-4; % accuracy 
+Tol = .1e-3; % accuracy 
 ItMax=50; % max number of iterations
 NodesMax=500000; % max number of nodes
-SkinnyTriangle=3; % sinny triangle definition
+SkinnyTriangle=3; % skinny triangle definition
 [nrts,rts,rtsM] = GRPF(NewNodesCoord, Tol, ItMax, NodesMax, SkinnyTriangle, Gparams,bc);
 Uu1 = xe+yb*1i;
 while nrts<=1.2*N	% might need some more roots, for look at adjacent regions moving along imaginary axis
@@ -120,7 +123,7 @@ for i = 1:length(rts)
 		if isnan(xn1)
 			[xn1,feVal1,itt1,~] = rootYasmin(mp(xtry(1)),ul,lr,Gparams,false);
 		end
-          [xn2,feVal2,itt2,~] = rootYasmin(xtry(2),ul,lr,Gparams,false);
+		[xn2,feVal2,itt2,~] = rootYasmin(xtry(2),ul,lr,Gparams,false);
 		if isnan(xn2)
 			[xn2,feVal2,itt2,~] = rootYasmin(mp(xtry(2)),ul,lr,Gparams,false);
 		end
@@ -129,8 +132,7 @@ for i = 1:length(rts)
 		itts = [itts, itt1, itt2];
 		RootIsMP = [RootIsMP, 0, 0];
 	end
-end
-	
+end		
 [~,id] = sort(imag(Roots));     % sort roots on imaginary part
 Roots = Roots(id);
 fnVal = fnVal(id);
@@ -139,17 +141,19 @@ RootIsMP = RootIsMP(id);
 
 % if you plot here, will need to set the plot limits dynamically for
 % current problem definition!
-Uu1 = 5-.5i;
-Ll1 = 25i - .5;
+Uu1 = 4.1-.2i;
+Ll1 = 30i - .2;
+Uu1 = double(max(real(Roots))*1.1-.2i);
+Ll1 = double(max(imag(Roots))*1.1i+min(real(Roots))*1.1-.05);
 
 if pltIt                % plot the roots on top of a contour plot of log(abs(f(mu)+1) 
     figure(pltIt);
     plotEigEq(Uu1+.1-.2i,Ll1,Gparams)
     hold on;
-    plot(1i*real(Roots)+imag(Roots),'.r');
-     ylabel('real(\mu)');
-    xlabel('imag(\mu)');
-    title('Root (\mu) diagram')
+    plot(1i*real(Roots)+imag(Roots),'*r');
+     ylabel('\bfreal(\mu)');
+    xlabel('\bfimag(\mu)');
+    title('\bfRoot (\mu) diagram')
     %axis([-.5,40,-.5,2.5]);
     hold off;
 end
